d67e34610bb71a1d9cea01dfaf12b62a8bf6ac24
suntao <sun.tao@lifecycle.cn>
2023-01-16 22:45:19 +0800

    1

---
+++ src/main/java/cn/joyjoyboy/code/springcode/CodeApplication.java
@@ -0,0 +1,13 @@
+package cn.joyjoyboy.code.springcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeApplication.class, args);
+    }
+
+}
